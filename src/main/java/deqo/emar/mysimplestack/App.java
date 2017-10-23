1e4a3805abb8eb8e03f0f5accd0868fd6cc386a7
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2017-10-24 00:03:25 +0200

    Initial commit
    Use the standard quickstart archetype.

---
+++ src/main/java/deqo/emar/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.emar.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
