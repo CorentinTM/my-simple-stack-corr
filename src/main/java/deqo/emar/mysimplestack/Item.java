b7eb0501a02d8828e1f5aca420ef3478ab298a49
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2017-10-24 00:55:18 +0200

    Item.java: Add a constructor and getter/setter

--- src/main/java/deqo/emar/mysimplestack/Item.java
+++ src/main/java/deqo/emar/mysimplestack/Item.java
@@ -4,4 +4,16 @@ package deqo.emar.mysimplestack;
  * Created by emartin on 24/10/17.
  */
 public class Item {
+    private Object object;
+
+    public Item (Object object) {
+        this.object = object;
+    }
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
 }
